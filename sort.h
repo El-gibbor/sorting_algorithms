e48d9f4cb9a907100f92da58799bbe6d40838381
El-gibbor <chiagoziem11@yahoo.com>
2023-09-25 22:50:32 +0400

    sorts in asceding order using selections sort

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void swap(listint_t *node1, listint_t *node2, listint_t **head);
 void quick_sort(int *array, size_t size);
 int partition(int *arr, int start_idx, int last_idx, int size);
 void quick(int *arr, int start_idx, int last_idx, int size);
-
+void selection_sort(int *array, size_t size);
+int index_of_min(int *arr, int start_index, size_t size);
 #endif
