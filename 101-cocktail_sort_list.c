555b34d22fd70a2e1f367d0f2375cd584415480a
El-gibbor <chiagoziem11@yahoo.com>
2023-09-26 00:36:25 +0400

    betty validated

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -8,37 +8,37 @@
  */
 void cocktail_sort_list(listint_t **list)
 {
-    int swapped;
-    listint_t *current;
+	int swapped;
+	listint_t *current;
 
-    if (!list || !*list)
-        return;
+	if (!list || !*list)
+		return;
 
-    do {
-        swapped = 0;
-        for (current = *list; current->next != NULL; current = current->next)
-        {
-            if (current->n > current->next->n)
-            {
-                swap_nodes(list, &current, &current->next);
-                swapped = 1;
-                print_list(*list);
-            }
-        }
-        if (!swapped)
-            break;
+	do {
+		swapped = 0;
+		for (current = *list; current->next != NULL; current = current->next)
+		{
+			if (current->n > current->next->n)
+			{
+				swap_nodes(list, &current, &current->next);
+				swapped = 1;
+				print_list(*list);
+			}
+		}
+		if (!swapped)
+			break;
 
-        swapped = 0;
-        for (; current->prev != NULL; current = current->prev)
-        {
-            if (current->n < current->prev->n)
-            {
-                swap_nodes(list, &current, &current->prev);
-                swapped = 1;
-                print_list(*list);
-            }
-        }
-    } while (swapped);
+		swapped = 0;
+		for (; current->prev != NULL; current = current->prev)
+		{
+			if (current->n < current->prev->n)
+			{
+				swap_nodes(list, &current, &current->prev);
+				swapped = 1;
+				print_list(*list);
+			}
+		}
+	} while (swapped);
 }
 
 /**
@@ -49,19 +49,19 @@ void cocktail_sort_list(listint_t **list)
  */
 void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
 {
-    listint_t *prev_node1 = (*node1)->prev;
-    listint_t *next_node2 = (*node2)->next;
+	listint_t *prev_node1 = (*node1)->prev;
+	listint_t *next_node2 = (*node2)->next;
 
-    if (prev_node1 != NULL)
-        prev_node1->next = *node2;
-    else
-        *list = *node2;
+	if (prev_node1 != NULL)
+		prev_node1->next = *node2;
+	else
+		*list = *node2;
 
-    (*node2)->prev = prev_node1;
-    (*node2)->next = *node1;
-    (*node1)->prev = *node2;
-    (*node1)->next = next_node2;
+	(*node2)->prev = prev_node1;
+	(*node2)->next = *node1;
+	(*node1)->prev = *node2;
+	(*node1)->next = next_node2;
 
-    if (next_node2 != NULL)
-        next_node2->prev = *node1;
+	if (next_node2 != NULL)
+		next_node2->prev = *node1;
 }
