4bac6e0143f02a12cacdc6587c670c1921764c2f
El-gibbor <chiagoziem11@yahoo.com>
2023-09-25 22:37:32 +0400

    updated header with all func signatures

--- sort.h
+++ sort.h
@@ -23,5 +23,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t *node1, listint_t *node2, listint_t **head);
+void quick_sort(int *array, size_t size);
+int partition(int *arr, int start_idx, int last_idx, int size);
+void quick(int *arr, int start_idx, int last_idx, int size);
 
 #endif
