fe072eb6e652a9e07a6fe0bb175f433b6871c3e4
El-gibbor <chiagoziem11@yahoo.com>
2023-09-26 01:32:45 +0400

    cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,67 +1,56 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list of ints in ascending order
- *                      using the Cocktail Shaker sort algorithm.
- * @list: A pointer to a pointer to the head of the list.
+ * swapme - swap the nodes themselves.
+ * @current: pointer.
+ * @current_old: pointer.
+ * @list: doubly linked list
  */
-void cocktail_sort_list(listint_t **list)
+void swapme(listint_t *current, listint_t *current_old, listint_t **list)
 {
-	int swapped;
-	listint_t *current;
+	listint_t *temp1 = current->next;
+	listint_t *temp2 = current_old->prev;
 
-	if (!list || !*list)
-		return;
-
-	do {
-		swapped = 0;
-		for (current = *list; current->next != NULL; current = current->next)
-		{
-			if (current->n > current->next->n)
-			{
-				swap_nodes(list, &current, &current->next);
-				swapped = 1;
-				print_list(*list);
-			}
-		}
-		if (!swapped)
-			break;
-
-		swapped = 0;
-		for (; current->prev != NULL; current = current->prev)
-		{
-			if (current->n < current->prev->n)
-			{
-				swap_nodes(list, &current, &current->prev);
-				swapped = 1;
-				print_list(*list);
-			}
-		}
-	} while (swapped);
+	if (temp1 != NULL)
+		temp1->prev = current_old;
+	if (temp2 != NULL)
+		temp2->next = current;
+	current->prev = temp2;
+	current_old->next = temp1;
+	current->next = current_old;
+	current_old->prev = current;
+	if (*list == current_old)
+		*list = current;
+	print_list(*list);
 }
 
 /**
- * swap_nodes - Swaps two nodes in a doubly linked list.
- * @list: A pointer to a pointer to the head of the list.
- * @node1: A pointer to the first node.
- * @node2: A pointer to the second node.
+ * cocktail_sort_list - cocktail_sort_list
+ *
+ * @list: doubly linked list
  */
-void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
+void cocktail_sort_list(listint_t **list)
 {
-	listint_t *prev_node1 = (*node1)->prev;
-	listint_t *next_node2 = (*node2)->next;
+	listint_t *check = *list, *first = NULL, *last = NULL;
 
-	if (prev_node1 != NULL)
-		prev_node1->next = *node2;
-	else
-		*list = *node2;
-
-	(*node2)->prev = prev_node1;
-	(*node2)->next = *node1;
-	(*node1)->prev = *node2;
-	(*node1)->next = next_node2;
-
-	if (next_node2 != NULL)
-		next_node2->prev = *node1;
+	if (!list || !(*list)->next)
+		return;
+	do {
+		while (check->next)
+		{
+			if (check->n > check->next->n)
+				swapme(check->next, check, list);
+			else
+				check = check->next;
+		}
+		last = check;
+		while (check->prev != first)
+		{
+			if (check->n < check->prev->n)
+				swapme(check, check->prev, list);
+			else
+				check = check->prev;
+		}
+		first = check;
+	} while (first != last);
 }
